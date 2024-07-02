f7699f9206a0c2cc5f6fd21f17619f26225c052b
N1krat <bogdanbraniste2007@gmail.com>
2024-07-02 23:54:03 +0300

    added mute button

--- main.py
+++ main.py
@@ -50,6 +50,9 @@ def resize_image(image_path, width, height):
 def volume(value): 
     volume.SetMasterVolume(volume_slider.get() / 100, None)
 
+def mute(): 
+    spotilib.mute()
+
 
 # Load buttn images
 button_width, button_height = 50, 50
@@ -57,16 +60,19 @@ play_image = resize_image("img/play.png", button_width, button_height)
 pause_image = resize_image("img/pause.png", button_width, button_height)
 next_image = resize_image("img/fast-forward.png", button_width, button_height)
 rewind_image = resize_image("img/rewind-button.png", button_width, button_height)
+mute_image = resize_image("img/volume-mute.png", button_width, button_height)
 
 # Creting bttns
 play_pause_button = tk.Button(app, image=play_image, command=toggle_play_pause)
 next_button = tk.Button(app, image=next_image, command=forward)
 rewind_button = tk.Button(app, image=rewind_image, command=rewind)
+mute_button = tk.Button(app, image=mute_image, command=mute)
 
 # anti-garbage collectin
 play_pause_button.image = play_image
 next_button.image = next_image
 rewind_button.image = rewind_image
+mute_button.image = mute_image
 
 # Bttn positions and padding
 print(spotilib.song())
@@ -77,6 +83,7 @@ bottom_frame.pack(side=tk.BOTTOM, pady=20)
 rewind_button.pack(side=tk.LEFT, padx=10)  
 play_pause_button.pack(side=tk.LEFT, padx=10)
 next_button.pack(side=tk.LEFT, padx=10)
+mute_button.pack(side=tk.LEFT, padx=10)
 
 volume_slider = tk.Scale(app, from_=0, to=100, orient=tk.VERTICAL, command=volume)
 volume_slider.pack(padx=10, pady=10, fill=tk.Y)
